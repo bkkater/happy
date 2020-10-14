3e54597bf457d254ec7f825a5cdd827837a200fa
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-14 11:34:05 -0300

    first commit

---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path='/' exact component={Landing}/>
+            <Route path='/OrphanagesMap' component={OrphanagesMap}/>
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
