3165172aa8fc130ccb4ae306324d365fcde6e7be
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-18 13:31:43 -0300

    [add] new migration images

---
+++ server/src/database/migrations/1603038683173-create_images.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImages1603038683173 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import OrphanagesController from './controllers/OrphanagesController'
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index)
+routes.get('/orphanages/:id', OrphanagesController.show)
 routes.post('/orphanages', OrphanagesController.create)
 
 export default routes;
