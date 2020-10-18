90bc89b608cf1ae00f6819156ef3dd7db3515464
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-18 13:22:33 -0300

    MVC config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import './database/connection';
+import routes from './routes'
+
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response)=>{
-    return response.json({"message": "Hello"})
-})
 
 app.listen(3333, () => {
     console.log('Server started! Port: 3333!');
-  });
+});
